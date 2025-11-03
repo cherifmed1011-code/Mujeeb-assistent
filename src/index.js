0c651ffcf3334c7b4833f57e851cced38da36759
cherifmed1011-code <cherifmed1011@gmail.com>
2025-11-03 08:43:08 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.post("/twilio/whatsapp/webhook", async (req, res) => {
 
     // 🔹 طلب Gemini API
     const geminiResponse = await axios.post(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
       {
         contents: [
           {
