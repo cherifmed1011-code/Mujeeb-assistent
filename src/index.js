a00b1e954a2daa64da3d871c5f38401f4442e168
cherifmed1011-code <cherifmed1011@gmail.com>
2025-11-04 09:07:22 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ app.post("/twilio/whatsapp/webhook", async (req, res) => {
     const groqResponse = await axios.post(
       "https://api.groq.com/openai/v1/chat/completions",
       {
-        model: "llama3-70b-8192", // يمكنك تغييره إلى llama3-70b إذا أردت أداء أقوى
+        model: "llama-3.1-8b-instant", // يمكنك تغييره إلى llama3-70b إذا أردت أداء أقوى
         messages: [
           {
             role: "system",
